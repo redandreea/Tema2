a652d881e47969e69a3a2add7771b597c089bee5
redandreea <redandreea@gmail.com>
2022-12-12 00:29:06 +0200

    Primul commit din Tema2

---
+++ src/Main.java
@@ -0,0 +1,191 @@
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+  /*this is what should be printed out:
+  this is your first number:
+12
+12
+25
+Numarul introdus este mai mic decat 101*/
+
+
+
+  /*public static void exB1() {
+         Scanner firstNr = new Scanner(System.in);
+         System.out.println("this is your first number: ");
+         int number = firstNr.nextInt();
+         System.out.println(number);
+    System.out.println((int)(Math.random()*101));
+        int randomNumber = 101;
+
+
+        //youtube source: https://www.youtube.com/watch?v=0jTCfOHtYaQ&ab_channel=NathanSchutz
+
+        if(number < randomNumber){
+            System.out.println("Numarul introdus este mai mic decat " + randomNumber); } */
+
+
+ /*
+
+
+  public static void exB2() {
+     Scanner deCitit = new Scanner(System.in);
+        System.out.println("Introdu orice caracter: ");
+        char scrieCeva = deCitit.next().charAt(0);
+        System.out.println("primul character introdus este: " + scrieCeva);
+        System.out.println("Introdu primul numar: ");
+        int primulNumar = deCitit.nextInt();
+        System.out.println("primul numar introdus este: " + primulNumar);
+        System.out.println("Introdu al doilea numar: ");
+        int numarulDoi = deCitit.nextInt();
+        System.out.println("al doilea numar introdus este: " + numarulDoi);
+
+
+
+
+        switch (scrieCeva) {
+            case 'a':
+                System.out.println("Suma este: " + (primulNumar + numarulDoi));
+                break;
+            case 's':
+                System.out.println("Scaderea este: " + (primulNumar - numarulDoi));
+                break;
+            case 'i':
+                System.out.println("Inmultirea este: " + (primulNumar * numarulDoi));
+                break;
+            case 'p':
+                System.out.println("Impartirea este " + (primulNumar / numarulDoi));
+                break;
+            case 'm':
+                System.out.println("Modulul este " + (primulNumar % numarulDoi));
+                break;
+            default:
+                System.out.println("Eroare!"); */
+
+
+            /*ar trebui sa arate :
+            Introdu orice caracter:
+a
+primul character introdus este: a
+Introdu primul numar:
+5
+primul numar introdus este: 5
+Introdu al doilea numar:
+7
+al doilea numar introdus este: 7
+Suma este:12*/
+
+
+
+/* public static void exB3 () {
+ int [][] numereleMele= {{77, 33, 22},
+                                {15, 14, 19},
+                                {40, 50, 60},
+                                {62, 63, 64},
+                                {55, 88, 20}};
+
+System.out.println(numereleMele [0][0]);
+        System.out.println(numereleMele [0][2]);
+        System.out.println(numereleMele [4][0]);
+        System.out.println(numereleMele[4][2]); */
+
+
+
+
+
+  /*ExB4
+
+
+    char[] abecedar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+        for (int i=0; i<abecedar.length; i+=2) {
+            System.out.println(abecedar[i]);
+        }
+
+
+
+
+
+   /* ex B5
+
+      double[] doubleArray = new double[9];
+
+        doubleArray[0] = 1.5;
+        doubleArray[1] = 2.5;
+        doubleArray[2] = 4.5;
+        doubleArray[3] = 6.5;
+        doubleArray[4] = 5.5;
+        doubleArray[5] = 7.5;
+        doubleArray[6] = 8.5;
+        doubleArray[7] = 10.5;
+        doubleArray[8] = 3.5;
+
+        for (int i = 0; i < 9; i++) {
+            if (i == 4.5) {
+                break;
+            }
+            System.out.println("element este egal cu " + doubleArray[i]); */
+
+
+  /* exB6- it should print out:
+98
+100
+90
+93
+95
+92
+ Index valid
+
+
+int[] note = {98, 100, 83, 90, 93, 95, 92, 91, 94};
+        for(int i=0; i<note.length; i++) {
+            if(i==2) {
+             continue;
+            } else if(i==7){
+                System.out.println(" Index valid ");
+                break;
+}
+System.out.println(note[i]);
+
+
+
+
+
+   /* exB7 (){
+    ArrayList<Integer> numbers =new ArrayList<Integer>();
+        numbers.add(new Integer(10));
+        numbers.add(new Integer(20));
+        numbers.add(new Integer(30));
+        numbers.add(new Integer(40));
+        numbers.add(new Integer(50));
+
+        System.out.println(numbers.get(0)+ numbers.get(1)+ numbers.get(2)+ numbers.get(3)+numbers.get(4)); */
+
+
+
+
+
+  /*ExB8- it should show the pets array is empty and ArrayList is empty.
+    String[] pets ={};
+        if (pets.length ==0) {
+            System.out.println("the pets array is empty");
+        }else{
+            System.out.println("the pets array is not empty");
+
+
+  ArrayList<String> petsArraylist =new ArrayList<>();
+
+        if (petsArraylist == null || petsArraylist.size() == 0) {
+            System.out.println("ArrayList is empty.");
+        } else {
+            System.out.println("ArrayList is not empty."); */
+
+
+}
+
